919eb2ab8236a2edabe47f1a025fba66f9effcec
Retrobox-BR <70717349+Retrobox-BR@users.noreply.github.com>
2020-11-20 21:01:25 -0300

    Delete Limpar Sistema.sh

--- emuelec/scripts/custom_scripts/nao_mexa/assistencia/Limpar Sistema.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -R /storage/downloads/1439All05112020-master/ >/dev/null 2>&1
-rm -R /storage/downloads/test/ >/dev/null 2>&1
- echo "Limpando Sistema... "
- systemctl restart emustation
